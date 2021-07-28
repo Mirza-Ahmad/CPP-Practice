15ceedf4d1173d8aeb845ad6bb8fd528e89ea4fc
Mirza-Ahmad <mirzaahmadawais1@gmail.com>
2021-07-28 11:53:40 +0000

    first commit

---
+++ rec.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int fab(int n);
+int main(void)
+{
+   int n, c=0;
+   cout<<"Enter the size: ";
+   cin>>n;
+   for(int i=1; i<=n; i++)
+   {
+     cout<<fab(c)<<" ";
+     c++;
+   }
+     return 0;
+}
+int fab(int n)
+{
+    if(n==0 || n==1)
+      return n;
+    else
+      return (fab(n-1) + fab(n-2));
+}
